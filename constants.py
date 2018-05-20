3ca36f12b8a7d215d2d23abb811e4b318afa6df5
Amit Prasad <amitprasad53121@gmail.com>
2018-05-20 12:25:50 +0100

    message topic and status messages added

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 20 11:51:35 2018
+
+@author: amit
+"""
+
+topic = ('FORECAST_CONSUMPTION', 'FORECAST_GENERATION', 'EXIT')
+
+
+satus = {'500':'INTERNAL SERVER ERROR',
+         '400': 'BAD REQUEST'
+        }
